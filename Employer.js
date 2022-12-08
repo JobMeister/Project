eeecbfbf20008b101ed1795592932df66f62d656
Daniel-Arvili <116496118+Daniel-Arvili@users.noreply.github.com>
2022-12-08 16:49:29 +0200

    Add files via upload

---
+++ Employer.js
@@ -0,0 +1,11 @@
+   class  Employer{ // Empolyer class
+    constructor(first_name,last_name,age,email,password,namecompany){ // ctor
+       this.first_name=first_name;
+       this.last_name=last_name;
+       this.age=age;
+       this.email=email;
+       this.password=password;
+       this.namecompany=namecompany;
+   }
+    }
+    
---
+++ worksearcher.js
@@ -0,0 +1,18 @@
+ class  WorkSeracher{ // work searcher class
+    
+    constructor(first_name,last_name,age,email,password,gender,main_profession,favorite_work){ // ctor
+    this.first_name=first_name;
+    this.last_name=last_name;
+    this.age=age;
+    this.email=email;
+    this.password=password;
+    this.gender=gender;
+    this.main_profession=main_profession;
+    this.favorite_work=favorite_work;
+}
+
+ }
+
+
+
+
