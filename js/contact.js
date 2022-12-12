d4bb99d9c4fa908eb7da61cf073a519fdd7ee69b
tomere13 <98044440+tomere13@users.noreply.github.com>
2022-12-12 15:23:54 +0200

    Contact feature is working

--- js/contact.js
+++ js/contact.js
@@ -1,21 +1,24 @@
-class contact{
-    name;
-    email;
-    theme;
-    content;
+// class contact{
+//     name;
+//     email;
+//     theme;
+//     content;
 
 
-    constructor(client_name, client_email, client_theme,client_content){
-        this.name = client_name;
-        this.email = client_email;
-        this.theme = client_theme;
-        this.content = client_content;
-    } 
+//     constructor(client_name, client_email, client_theme,client_content){
+//         this.name = client_name;
+//         this.email = client_email;
+//         this.theme = client_theme;
+//         this.content = client_content;
+//     } 
 
 
-};
+// };
 
-var i =0;
+var x = Number(localStorage.getItem("num_of_msg"));    
+var i;
+if(x == null || i ==0) i =0;
+else i=x;
 
 $("#Send").click(function(){
     Get_msg(i);
@@ -23,9 +26,15 @@ $("#Send").click(function(){
 });
 
 function Get_msg(i){
+    // var a = new contact($("#name").val(),$("#email").val(),$("#subject").val(),$("#message").val());
 
-    localStorage.setItem("Client" + i.toString(), contact($("#name").val(),$("#email").val(),$("#subject").val(),$("#message").val() ));
-}
+    localStorage.setItem("Client " + i + " name", $("#name").val());
+    localStorage.setItem("Client " + i + " email", $("#email").val());
+    localStorage.setItem("Client " + i + " subject", $("#subject").val());
+    localStorage.setItem("Client " + i + " message", $("#message").val());
+    
+    localStorage.setItem("num_of_msg",i+1);
+};
 
 
 
