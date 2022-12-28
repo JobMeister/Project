ed39b4e08bbd91ba646f70a69d898d1c4b6ddcac
Daniel Arvili <danarvili@gmail.com>
2022-12-28 19:24:39 +0200

    fixed

--- src/admin.js
+++ src/admin.js
@@ -81,9 +81,11 @@ $(document).ready(function(){
   $("#reportsBtn").click(function(){
 
    
-    $("#reports").toggle('drop');
     $("#editdiv").hide();
     $("#admin_msg").hide();
+    $("#not_admin_conads").hide();
+    $("#admin_conads").hide();
+    $("#reports").toggle('drop');
 
 
     return false;
@@ -92,10 +94,11 @@ $(document).ready(function(){
   
   $("#msg_btn").click(function(){
 
-   
-      $("#admin_msg").toggle('drop');
       $("#editdiv").hide();
       $("#reports").hide();
+      $("#not_admin_conads").hide();
+      $("#admin_conads").hide();
+      $("#admin_msg").toggle('drop');
 
       return false;
     
@@ -103,9 +106,11 @@ $(document).ready(function(){
 
   $("#edit_btn").click(function(){
     
-      $("#editdiv").toggle('drop');
       $("#admin_msg").hide();
       $("#reports").hide();
+      $("#not_admin_conads").hide();
+      $("#admin_conads").hide();
+      $("#editdiv").toggle('drop');
 
 
       return false;
@@ -121,11 +126,11 @@ $(document).ready(function(){
   
 })
 $("#notconfirm_ads").click(function(){
-  $("#not_admin_conads").toggle('drop');
   $("#admin_conads").hide();
   $("#editdiv").hide();
   $("#admin_msg").hide();
   $("#reports").hide();
+  $("#not_admin_conads").toggle('drop');
   return false;
 
 })
@@ -137,31 +142,7 @@ var editB = $("#editNow");
 
 var NewText = "JobMeister הוא פלטפורמת הדרושים הטובה ביותר של ישראל וזאת בזכות מערכת התאמת מועמד למעסיק הטובה ביותר. מטרת האתר היא לעזור למועמדים מכלל תחומי התעסוקה למצוא את העבודה הטובה והמתאימה להם, ובמקביל לעזור למעסיקים למצוא במהירות ויעילות את העובדים הטובים והמתאימים ביותר.";
 })
-// editB.click( function() {
-//   NewText = $("#Newtext").val();
-//   localStorage.setItem("About", NewText);
-//   $("#editP").html(NewText);
-//   alert("Saved");
 
-//   } );
-
-// var num_msg = localStorage.getItem("num_of_msg");
-// for (var index = 0; index < num_msg; index++) {
-//   let name = localStorage.getItem("Client " + index + " name");
-//   let email = localStorage.getItem("Client " + index + " email");
-//   let subject = localStorage.getItem("Client " + index + " subject");
-//   let message = localStorage.getItem("Client " + index + " message");
-//   $("#admin_msg").append("<strong>Number of message is - </strong>" + (index+1));
-//   $("#admin_msg").append("<p> Name: " + name + "<br>" +"Email: " + email + "<br>" + "Subject: " + subject + "<br>" + "Message: " + message + "</p>" );
-  
-// };
-
-
-// var num_ads = localStorage.getItem("num_of_ads");
-
-// $("#reports").append("<strong>Number of messeges is - "+  index +"</strong>");
-// $("#reports").append("<strong><p>Number of ads is - "+  num_ads +"</strong></p>");
-// // $("#reports").append(" <img src="img/adv.png"/> ");
 
 
  
