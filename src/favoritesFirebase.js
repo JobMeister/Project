fba870e426f6efe633bc4dbe16c95fafb9882793
tomere13 <98044440+tomere13@users.noreply.github.com>
2023-03-16 14:57:16 +0200

    pic fix

--- src/favoritesFirebase.js
+++ src/favoritesFirebase.js
@@ -104,7 +104,7 @@ getDocs(adColRef).then((snapshot) => {
             if (Ads[index].accepted == true && Ads[index].id==allSavedAds[i].idOfAds && allSavedAds[i].Saveremail == logEmail) {
               document.querySelector("#adC").innerHTML = ++adCount;
               $("#try1").append(
-                "<div class='Added col-md-4'> <div class='card mb-4 box-shadow'><img class='card-img-top' src='/dist/img/occpics/occ" +
+                "<div class='Added col-md-4'> <div class='card mb-4 box-shadow'><img class='card-img-top' src='/img/occpics/occ" +
                   Ads[index].imgid +
                   ".jpeg' alt='Thumbnail [100%x225]' style='height: 225px; width: 100%; display: block;' data-holder-rendered='true'><div class='card-body'> <h5 id='cardHeader' dir='rtl'><b>" +
                   Ads[index].title +
@@ -136,7 +136,7 @@ getDocs(adColRef).then((snapshot) => {
           document.querySelector("#WSdep").innerHTML = Ads[index].dep;
           document.querySelector("#WSpercent").innerHTML = Ads[index].percent;
           document.querySelector("#WSimg").src =
-            "/dist/img/occpics/occ" + Ads[index].imgid + ".jpeg";
+            "/img/occpics/occ" + Ads[index].imgid + ".jpeg";
         })
       }
     }
